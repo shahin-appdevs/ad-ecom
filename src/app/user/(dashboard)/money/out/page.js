bce09b83bc22b00ccc1f87e4754f8249bd1422e0
Shahin Hossain <shahin@appdevs.team>
2026-02-16 16:32:43 +0600

    update: money out responsive, realtime trx update, ongoing btn disabled

--- src/app/user/(dashboard)/money/out/page.js
+++ src/app/user/(dashboard)/money/out/page.js
@@ -1,15 +1,17 @@
+"use client";
 // Components
 import MoneyOutSection from "@/components/dashboard/pages/moneyOut/moneyOut";
 import MoneyOutHistorySection from "@/components/dashboard/pages/moneyOut/moneyOutHistory";
-
+import { useState } from "react";
 
 export default function MoneyOut() {
+    const [isRefetch, setRefetch] = useState(false);
     return (
         <>
             <div className="grid grid-cols-4 gap-4">
                 <div className="xl:col-span-4 col-span-4 space-y-4">
-                    <MoneyOutSection />
-                    <MoneyOutHistorySection />
+                    <MoneyOutSection setRefetch={setRefetch} />
+                    <MoneyOutHistorySection isRefetch={isRefetch} />
                 </div>
             </div>
         </>
