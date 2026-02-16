71b474796191fbbd9160922b86a0919439ab8892
Shahin Hossain <shahin@appdevs.team>
2026-02-16 17:13:29 +0600

    update: request money realtime trx, reponsive, disabled button ongoing req

--- src/app/user/(dashboard)/money/request/page.js
+++ src/app/user/(dashboard)/money/request/page.js
@@ -1,15 +1,17 @@
+"use client";
 // Components
 import RequestMoneySection from "@/components/dashboard/pages/requestMoney/requestMoney";
 import RequestMoneyHistorySection from "@/components/dashboard/pages/requestMoney/requestMoneyHistory";
-
+import { useState } from "react";
 
 export default function RequestMoney() {
+    const [isRefetch, setRefetch] = useState(false);
     return (
         <>
             <div className="grid grid-cols-4 gap-4">
                 <div className="xl:col-span-4 col-span-4 space-y-4">
-                    <RequestMoneySection />
-                    <RequestMoneyHistorySection />
+                    <RequestMoneySection setRefetch={setRefetch} />
+                    <RequestMoneyHistorySection isRefetch={isRefetch} />
                 </div>
             </div>
         </>
