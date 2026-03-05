3e87815496bf5719db47e8543378f6a753dac1b9
Shahin Hossain <shahin@appdevs.team>
2026-03-05 11:23:12 +0600

    update: language money exchange trx

--- src/components/dashboard/pages/transactions/moneyExchange.js
+++ src/components/dashboard/pages/transactions/moneyExchange.js
@@ -1,8 +1,9 @@
-'use client';
-import { useState, useEffect } from 'react';
+"use client";
+import { useState, useEffect } from "react";
 import { exchangeGetAPI } from "@root/services/apiClient/apiClient";
 import { toast } from "react-hot-toast";
-import { useRouter } from 'next/navigation';
+import { useRouter } from "next/navigation";
+import { useTranslations } from "next-intl";
 
 function SkeletonRow() {
     return (
@@ -42,6 +43,7 @@ export default function ExchangeMoneyHistorySection() {
     const [apiLoading, setApiLoading] = useState(true);
     const [transactions, setTransactions] = useState([]);
     const router = useRouter();
+    const t = useTranslations("Dashboard.transactions.moneyExTransaction");
 
     useEffect(() => {
         const fetchExchangeData = async () => {
@@ -52,13 +54,16 @@ export default function ExchangeMoneyHistorySection() {
             } catch (error) {
                 const errorMessage = error.response?.data?.message?.error?.[0];
                 toast.error(errorMessage);
-                if (errorMessage === "Kindly complete your PIN setup before proceeding.") {
+                if (
+                    errorMessage ===
+                    "Kindly complete your PIN setup before proceeding."
+                ) {
                     router.push("/user/setup/pin");
                 }
                 setApiLoading(false);
             }
         };
-        
+
         fetchExchangeData();
     }, []);
 
@@ -79,39 +84,57 @@ export default function ExchangeMoneyHistorySection() {
     };
 
     const formatDate = (dateString) => {
-        if (!dateString) return '';
+        if (!dateString) return "";
         const date = new Date(dateString);
-        return date.toLocaleString('en-US', {
-            year: '2-digit',
-            month: '2-digit',
-            day: '2-digit',
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: true
+        return date.toLocaleString("en-US", {
+            year: "2-digit",
+            month: "2-digit",
+            day: "2-digit",
+            hour: "2-digit",
+            minute: "2-digit",
+            second: "2-digit",
+            hour12: true,
         });
     };
 
     return (
         <div className="bg-white rounded-[12px] p-7">
             <div className="flex flex-col md:flex-row md:items-center md:justify-between pb-3 mb-2 gap-3 border-b-[1.5px] border-[#F5F7FF]">
-                <h2 className="text-[16px] font-semibold">Money Exchange Logs</h2>
+                <h2 className="text-[16px] font-semibold">{t("title")}</h2>
             </div>
-            
+
             {apiLoading ? (
                 <div className="table-wrapper overflow-x-auto">
                     <table className="min-w-full divide-y divide-[#F5F7FF] whitespace-nowrap">
                         <thead>
                             <tr className="bg-[#F5F7FF] text-left text-sm text-color__paragraph">
-                                <th className="py-4 px-5 font-semibold">Trx</th>
-                                <th className="py-4 px-5 font-semibold">Request Amount</th>
-                                <th className="py-4 px-5 font-semibold">Payable</th>
-                                <th className="py-4 px-5 font-semibold">Exchange Rate</th>
-                                <th className="py-4 px-5 font-semibold">Fees & Charges</th>
-                                <th className="py-4 px-5 font-semibold">Exchange Amount</th>
-                                <th className="py-4 px-5 font-semibold">Current Balance</th>
-                                <th className="py-4 px-5 font-semibold">Status</th>
-                                <th className="py-4 px-5 font-semibold">Time & Date</th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("trx")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("requestAmount")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("payable")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("rate")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("fees")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("exchangeAmount")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("balance")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("status")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("time")}
+                                </th>
                             </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-[#F5F7FF]">
@@ -122,28 +145,50 @@ export default function ExchangeMoneyHistorySection() {
                     </table>
                 </div>
             ) : transactions.length === 0 ? (
-                <div className="text-center py-5">No exchange transactions found</div>
+                <div className="text-center py-5">{t("noTransactions")}</div>
             ) : (
                 <div className="table-wrapper overflow-x-auto">
                     <table className="min-w-full divide-y divide-[#F5F7FF] whitespace-nowrap">
                         <thead>
-                            <tr className="bg-[#F5F7FF] text-left text-sm text-color__paragraph">
-                                <th className="py-4 px-5 font-semibold">Trx</th>
-                                <th className="py-4 px-5 font-semibold">Request Amount</th>
-                                <th className="py-4 px-5 font-semibold">Payable</th>
-                                <th className="py-4 px-5 font-semibold">Exchange Rate</th>
-                                <th className="py-4 px-5 font-semibold">Fees & Charges</th>
-                                <th className="py-4 px-5 font-semibold">Exchange Amount</th>
-                                <th className="py-4 px-5 font-semibold">Current Balance</th>
-                                <th className="py-4 px-5 font-semibold">Status</th>
-                                <th className="py-4 px-5 font-semibold">Time & Date</th>
+                            <tr className="bg-[#F5F7FF] text-left rtl:text-right text-sm text-color__paragraph">
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("trx")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("requestAmount")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("payable")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("rate")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("fees")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("exchangeAmount")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("balance")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("status")}
+                                </th>
+                                <th className="py-4 px-5 font-semibold">
+                                    {t("time")}
+                                </th>
                             </tr>
                         </thead>
                         <tbody className="bg-white divide-y divide-[#F5F7FF]">
                             {transactions.map((transaction, index) => (
-                                <tr key={index}>
+                                <tr
+                                    dir="ltr"
+                                    className="rtl:text-right"
+                                    key={index}
+                                >
                                     <td className="py-3.5 px-5 whitespace-nowrap text-sm font-medium text-primary__color">
-                                        #{transaction.trx || 'N/A'}
+                                        #{transaction.trx || "N/A"}
                                     </td>
                                     <td className="py-3.5 px-5 whitespace-nowrap text-sm font-medium">
                                         {transaction.request_amount}
@@ -164,7 +209,9 @@ export default function ExchangeMoneyHistorySection() {
                                         {transaction.current_balance}
                                     </td>
                                     <td className="py-3.5 px-5 whitespace-nowrap">
-                                        <span className={`px-3 inline-flex text-[10px] leading-5 font-semibold rounded-[4px] capitalize ${getStatusColor(transaction.status)}`}>
+                                        <span
+                                            className={`px-3 inline-flex text-[10px] leading-5 font-semibold rounded-[4px] capitalize ${getStatusColor(transaction.status)}`}
+                                        >
                                             {transaction.status}
                                         </span>
                                     </td>
