b94cc0d2fa2d0b5ffd1323e1a356bf70ef8faf21
Shahin Hossain <shahin@appdevs.team>
2026-03-30 13:12:52 +0600

    removed: unused all product page

--- src/app/[locale]/(template)/product/page.js
+++
@@ -1,6 +0,0 @@
-export const dynamic = "force-dynamic";
-import AllProductSection from "@/components/pages/product/allProduct";
-
-export default function ProductPage() {
-    return <>{/* <AllProductSection /> */}</>;
-}
